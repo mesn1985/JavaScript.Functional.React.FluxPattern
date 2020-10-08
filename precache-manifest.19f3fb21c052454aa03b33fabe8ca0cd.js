a2f48b3b14f616ed8a089505ad54852f09d49878
Martin Edwin Shcjødt Nielsen <lightmaze16@hotmail.com>
2020-10-08 20:17:25 +0200

    Updates

---
+++ precache-manifest.19f3fb21c052454aa03b33fabe8ca0cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c32334d45789ed1ec8ba475f2f7d1a",
+    "url": "/JavaScript.Functional.React.FluxPattern/index.html"
+  },
+  {
+    "revision": "86df0a37921448490662",
+    "url": "/JavaScript.Functional.React.FluxPattern/static/css/2.3e972cad.chunk.css"
+  },
+  {
+    "revision": "422083f9330a3e73d736",
+    "url": "/JavaScript.Functional.React.FluxPattern/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "86df0a37921448490662",
+    "url": "/JavaScript.Functional.React.FluxPattern/static/js/2.9c9eeb74.chunk.js"
+  },
+  {
+    "revision": "422083f9330a3e73d736",
+    "url": "/JavaScript.Functional.React.FluxPattern/static/js/main.48e529dd.chunk.js"
+  },
+  {
+    "revision": "74ef744955c6fd0979aa",
+    "url": "/JavaScript.Functional.React.FluxPattern/static/js/runtime-main.55323d2a.js"
+  }
+]);
